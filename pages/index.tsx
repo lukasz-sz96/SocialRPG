0b712fc723ea4b35d895948a85e932530744113a
lukasz-sz96 <lukasz.szczesny96@gmail.com>
2021-06-02 00:19:45 +0200

    Display SignIn and SIgnOut buttons, display active session user

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,57 @@
 import React, { useEffect } from "react";
+import { useSession, signIn, signOut } from "next-auth/client";
+
 import axios from "axios";
 
 import { ExampleComponent } from "@components";
 
 const Home: React.FC = () => {
     //MOCK
-    useEffect(() => {
-        const fetch = async () => {
-            const newPlayer = await axios.post(
-                "http://localhost:3000/api/user/create",
-                {
-                    data: {
-                        name: "TestPlayer",
-                        password: "TestPassword",
-                        hp: 50,
-                    },
-                },
-            );
+    // useEffect(() => {
+    //     const fetch = async () => {
+    //         const newPlayer = await axios.post(
+    //             "http://localhost:3000/api/user/create",
+    //             {
+    //                 data: {
+    //                     name: "TestPlayer",
+    //                     password: "TestPassword",
+    //                     hp: 50,
+    //                 },
+    //             },
+    //         );
 
-            const getPlayer = await axios.get(
-                "http://localhost:3000/api/user/get",
-                {
-                    params: { name: "TestPlayer" },
-                },
-            );
-            console.log(newPlayer);
-            console.log(getPlayer);
-        };
-        fetch();
-    }, []);
-    return <ExampleComponent />;
+    //         const getPlayer = await axios.get(
+    //             "http://localhost:3000/api/user/get",
+    //             {
+    //                 params: { name: "TestPlayer" },
+    //             },
+    //         );
+    //         console.log(newPlayer);
+    //         console.log(getPlayer);
+    //     };
+    //     fetch();
+    // }, []);
+
+    const [session, loading] = useSession();
+
+    if (loading) {
+        return <h2>Loading session data...</h2>;
+    }
+    if (session) {
+        return (
+            <>
+                Signed in as {session.user.name} <br />
+                <button onClick={() => signOut()}>Sign out</button>
+                <ExampleComponent />
+            </>
+        );
+    }
+    return (
+        <>
+            Not signed in <br />
+            <button onClick={() => signIn()}>Sign in</button>
+        </>
+    );
 };
 
 export default Home;
