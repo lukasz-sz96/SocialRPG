b687659b22688a57fa53e275081a2cdd32484865
lukasz-sz96 <lukasz.szczesny96@gmail.com>
2021-06-19 20:33:49 +0200

    Extend color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
     purge: ["./pages/**/*.tsx", "./src/**/*.tsx"],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                base: "#23395b",
+                baseDark: "#161925",
+                primary: "#3b82f6",
+                secondary: "#cbf7ed",
+                error: "#d0a5c0",
+            },
+        },
     },
     variants: {
         extend: {},
